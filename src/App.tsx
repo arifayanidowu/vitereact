df3d1a050a54a14f4ac06e02b754e3f0363554b6
stizzle123 <arifayanidowu2@yahoo.com>
2022-10-22 12:13:10 +0100

    Update url for ios launch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const launchOnAndroidDevice = () => {
     const url =
-      "intent://vitereact.netlify.com/#Intent;scheme=com.seamless_hrms;package=com.seamless_hrms;end";
+      "intent://viteweb.netlify.app//#Intent;scheme=com.seamless_hrms;package=com.seamless_hrms;end";
     window.location.href = url;
   };
 
   const launchOnIosDevice = () => {
-    const url = "com.seamless_hrms://";
+    const url = "seamless_hrms://";
     window.location.href = url;
   };
 
@@ -20,7 +16,12 @@ function App() {
     <div className="App">
       <div style={{ display: "flex", gap: 10, flexWrap: "wrap" }}>
         <button onClick={launchOnAndroidDevice}>Android Device</button>
-        <button onClick={launchOnIosDevice}>iOS Device</button>
+        <button
+          onClick={launchOnIosDevice}
+          style={{ backgroundColor: "brown" }}
+        >
+          iOS Device
+        </button>
       </div>
     </div>
   );
